01518f1ce0fb62f2c24543258734dcf575544ef6
Maciej Kruk <mkruk@telr.com>
2015-03-06 17:11:04 +0800

    removed wordpress
    Decided setting up Wordpress locally is a bad idea for a variety of reasons:
    * need to maintain MySQL DB no matter what your project uses
    * need to worry about synchronization when using multiple servers
    * overall it's more costly than using paid wordpress hosting service

--- cookbooks/base/recipes/rails_server.rb
+++ cookbooks/base/recipes/rails_server.rb
@@ -10,21 +10,20 @@ group "rvm" do
   append true
 end
 
-ENV['PATH']="#{node[cookbook_name]['rvm_path']}:#{ENV['PATH']}"
+magic_shell_environment 'PATH' do
+  value "#{node[cookbook_name]['rvm_path']}:#{ENV['PATH']}"
+end
 
 include_recipe "nginx::source"
 
 config_name = "#{cookbook_name}-#{node.chef_environment}"
-
-blog_ip = search(:node, "run_list:recipe\[#{cookbook_name}\:\:wordpress\]").first['ec2']['public_ipv4']
-blog_url = "http://#{blog_ip}/blog/"
 
 template "/etc/nginx/sites-available/#{config_name}" do
   source "etc/nginx/sites-available/config.erb"
   action :create
   mode "0644"
   notifies :reload, "service[nginx]", :delayed
-  variables(blog_url: blog_url)
+  variables({cookbook_name: cookbook_name})
 end
 
 link "/etc/nginx/sites-enabled/#{config_name}" do
