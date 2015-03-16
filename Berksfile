45ca1b452e4fe425508a9499d8ebd8cbb8661293
Maciej Kruk <mkruk@telr.com>
2015-03-16 10:45:43 +0800

    fix rvm installation

--- Berksfile
+++ Berksfile
@@ -2,7 +2,7 @@ source "http://api.berkshelf.com"
 
 cookbook 'ntp'
 cookbook 'apt'
-cookbook 'rvm', git: 'https://github.com/fnichol/chef-rvm.git'
+cookbook 'rvm', git: 'https://github.com/martinisoft/chef-rvm.git'
 cookbook 'nginx'
 cookbook 'logrotate'
 cookbook 'magic_shell'
--- cookbooks/base/attributes/rails_server.rb
+++ cookbooks/base/attributes/rails_server.rb
@@ -5,6 +5,9 @@ normal['rvm']['user_default_ruby'] = node['rvm']['default_ruby']
 normal['rvm']['gems'][node['rvm']['default_ruby']] = [{name: "bundler"}, {name: "rake"}]
 normal['rvm']['gem_package']['rvm_string'] = node['rvm']['default_ruby']
 normal['rvm']['gpg']['keyserver'] = "hkp://keys.gnupg.net"
+normal['rvm']['gpg']['homedir'] = "/root"
+normal['rvm']['install_rubies'] = true
+
 
 # nginx
 normal['nginx']['version'] = '1.7.10'
